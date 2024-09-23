6bf14f21d5024449efe88f47a9739db12b65fecf
Hussnain Raza <hussnain444all@gmail.com>
2024-09-23 12:41:34 +0500

    integrated full story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+'use client'
 import CertificatesAndAwardsSection from '@/components/CertificatesAndAwards'
+import * as FullStory from '@fullstory/browser'
+import { useEffect } from 'react'
 import ContactSection from '../components/ContactSection'
 import ExperienceSection from '../components/ExperienceSection'
 import HeroSection from '../components/HeroSection'
@@ -6,6 +9,9 @@ import ProjectsSection from '../components/ProjectsSection'
 import SkillsSection from '../components/SkillsSection'
 
 export default function Home() {
+  useEffect(() => {
+    FullStory.init({ orgId: 'o-21ASQA-na1'});
+  }, []);
   return (
     <>
       <HeroSection />
